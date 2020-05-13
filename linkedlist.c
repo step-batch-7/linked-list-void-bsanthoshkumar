e52794f7da6e4fe621546b9c2520191383878245
santhosh <santhosh.kumar@thoughtworks.com>
2020-05-13 23:30:23 +0530

    changed returning failure to NULL in all remove functions

--- linkedlist.c
+++ linkedlist.c
@@ -98,7 +98,7 @@ Element remove_from_start(List_ptr list)
 {
   if (list->first == NULL)
   {
-    return Failure;
+    return NULL;
   }
 
   Node_ptr node = list->first;
@@ -116,7 +116,7 @@ Element remove_from_end(List_ptr list)
 {
   if (list->last == NULL)
   {
-    return Failure;
+    return NULL;
   }
 
   if (list->length == 1)
@@ -142,7 +142,7 @@ Element remove_at(List_ptr list, int position)
 {
   if (position < 0 || position >= list->length)
   {
-    return Failure;
+    return NULL;
   }
 
   if (position == 0)
