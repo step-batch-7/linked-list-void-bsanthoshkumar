9b9fc08ab33abac2582f77437333660a91070b96
santhosh <santhosh.kumar@thoughtworks.com>
2020-05-14 13:31:00 +0530

    added tests for insert_at function

--- test/linkedlist_test.c
+++ test/linkedlist_test.c
@@ -31,12 +31,35 @@ Boolean test_add_to_start()
 
   return flag;
 }
+
+Boolean test_insert_at()
+{
+  Boolean flag = True;
+  List_ptr list = create_list();
+  int *a = malloc(sizeof(int));
+  *a = 10;
+  add_to_list(list, a);
+  int *b = malloc(sizeof(int));
+  *b = 10;
+  add_to_list(list, b);
+  int *c = malloc(sizeof(int));
+  *c = 10;
+  flag = flag && assert_status_equal(insert_at(list, a, 1), Success);
+  flag = flag && assert_numbers_equal(list->length, 3);
+  flag = flag && assert_status_equal(*(int *)list->first->next->element, *c);
+  free(list);
+  free(a);
+
+  return flag;
+}
+
 void run_tests()
 {
   printf("running tests ....\n\n");
 
   it("Should insert element at end", &test_add_to_list);
   it("Should insert element at start", &test_add_to_start);
+  it("Should insert element at position", &test_insert_at);
 }
 
 int main()
